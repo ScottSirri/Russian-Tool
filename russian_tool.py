1dbd847922c461203e417fe3c9d9f704e5357b4e
Scott Sirri <scottsirri@wustl.edu>
2024-07-23 23:44:45 -0500

    Definitions don't quite work, test on the page for ебать, but I think I have declensions down now and conjugations will be a simple extension of that thanks to the general table-parsing framework I built

--- russian_tool.py
+++ russian_tool.py
@@ -1,10 +1,16 @@
 import requests
 import sys
 
+# Oooooo magic numbers :o
 DECL = 1001
 CONJ = 1002
 SIMILAR = 1003
 OTHER = 1004
+
+section_codes = { DECL : "DECL", 
+                 CONJ : "CONJ",
+                 SIMILAR : "SIMILAR",
+                 OTHER : "OTHER"}
 
 url = ''
 
@@ -73,13 +79,10 @@ def on_definitions(elem):
 def extract_defns(elem):
 
     lines = []
-    lis = elem.children
 
-    for li in lis:
-
+    for li in elem.children:
         if li.name == None:
             continue
-
         line = ''
         for kiddo in li.children:
             if(kiddo.name != 'a' and kiddo.name != None 
@@ -89,18 +92,23 @@ def extract_defns(elem):
 
         line = line.strip()
         lines.append(line)
-        print('\t' + line)
 
     return lines
 
 def get_subsection_type(elem):
+
     h4_elems = elem.find_all("h4")
+
     for elem in h4_elems:
+
         if not hasattr(elem, 'attrs'):
             continue
+
         if 'id' in elem.attrs:
             elem_id = elem['id']
 
+            # Sometimes theres weird suffixes on the id's, so it's more
+            # reliable to check for substring than exact match
             if "Declension" in elem_id:
                 return DECL
             elif "Conjugation" in elem_id:
@@ -110,21 +118,74 @@ def get_subsection_type(elem):
             else:
                 return OTHER
 
+def is_table(elem):
+    if is_valid_elem(elem):
+        if elem_is_contains(elem, 'class', 'NavFrame'):
+            return True
+    return False
+
+# Some attributes are multi-valued, so it's hard to tell whether you're
+# querying for an exact membership or membership. This tests both.
+def elem_is_contains(elem, key, query):
+    if not is_valid_elem(elem):
+        return False
+    if key not in elem.attrs:
+        return False
+    if type(elem[key]) == list:
+        return query in elem[key]
+    else:
+        return query == elem[key]
+
+# Returns a list of all Russian-language table contents
+def get_all_table_contents(table_elem):
+    
+    contents = []
+
+    if not elem_is_contains(table_elem, 'class', 'NavFrame'):
+        print("Non-table element has been passed to get_all_table_contents")
+        return
+
+    table_contents_elem = table_elem.find("div", "NavContent")
+
+    if table_contents_elem == None:
+        print("table contents not found")
+    else:
+        ru_cells = table_contents_elem.find_all("span", lang="ru")
+        for cell in ru_cells:
+            contents.append(cell.get_text())
+        return contents
+
+# If the elem is None or is an empty NavigableString, return False
+def is_valid_elem(elem):
+    if elem == None or not hasattr(elem, 'attrs'):
+        return False
+    return True
+
 def extract_decl(elem):
-    print("called extract_decl")
+
+    elem = elem.next_sibling
+
+    while not is_valid_elem(elem):
+        elem = elem.next_sibling
+
+    for elem_child in elem.children:
+        if is_table(elem_child):
+            return get_all_table_contents(elem_child)
 
 def extract_conj(elem):
+    # TODO : Implement
     print("called extract_conj")
 
 def extract_similar(elem):
+    # TODO : Implement
     print("called extract_similar")
 
 # Returns the definitions, declensions, conjugations, and related terms
 # (as applicable) in the Russian section of the given soup element.
 def wiki_decl(soup):
     defns = []
-    decl = []
-    conj = []
+    decls = []
+    conjs = []
     related = []
 
     # Finds the section header for Russian
@@ -138,14 +199,15 @@ def wiki_decl(soup):
     current_elem = russian_sec_search[0].parent
 
     while True:
-        current_elem = current_elem.next_sibling
 
         if current_elem == None:
             break
 
+        current_elem = current_elem.next_sibling
+
         # There are often empty 'NaviagableString's between elements.
         # This saves us from trying to query them
-        if not hasattr(current_elem, 'attrs'):
+        if not is_valid_elem(current_elem):
             continue
 
         # If we're now past the Russian section, terminate the search
@@ -157,7 +219,17 @@ def wiki_decl(soup):
 
         if on_new_subsection(current_elem):
             type_subsection = get_subsection_type(current_elem)
-            # Handle this
-            print("TYPE: " + str(type_subsection))
+            # TODO : Implement
+            print("SECTION FOUND: " + section_codes[type_subsection])
+            if type_subsection == DECL:
+                decls = extract_decl(current_elem)
+
+    print("\nDefinitions:")
+    for line in defns:
+        print("\t" + line)
+
+    print("\nDeclensions:")
+    for line in decls:
+        print("\t" + line)
 
 wiki_decl(soup_outer)
