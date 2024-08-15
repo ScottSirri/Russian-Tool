12f3b133e90dc4cf0d147aa857f7b89d138334c4
Scott Sirri <scottsirri@wustl.edu>
2024-08-14 20:49:20 -0400

    Incremental progress

--- syno_search.py
+++ syno_search.py
@@ -1,3 +1,5 @@
+import sys, requests
+
 cyrillic_key = "АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя"
 
 encoded_key = "%C0%E0%C1%E1%C2%E2%C3%E3%C4%E4%C5%E5%A8%B8%C6%E6%C7%E7%C8%E8%C9%E9%CA%EA%CB%EB%CC%EC%CD%ED%CE%EE%CF%EF%D0%F0%D1%F1%D2%F2%D3%F3%D4%F4%D5%F5%D6%F6%D7%F7%D8%F8%D9%F9%DA%FA%DB%FB%DC%FC%DD%FD%DE%FE%DF%FF"
@@ -15,4 +17,13 @@ def encode(string):
     return ret
 
 print(encode("тест"))
-    
+url_base_syn = "https://www.textsale.ru/index.php?sword="
+url = url_base_syn + encode("сердитый")
+r = None
+try:
+    r = requests.get(url)
+except:
+    print(f"search_defn: Link not valid (%s)" % url)
+    sys.exit()
+html_doc = r.text
+print(html_doc)
