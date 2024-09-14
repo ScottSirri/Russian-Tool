14e5f4722c2194b6fd065d3933cd400920c2e648
Scott Sirri <scottsirri@wustl.edu>
2024-09-13 19:11:39 -0500

    Some random things, clear button

--- gui.py
+++ gui.py
@@ -7,6 +7,8 @@ from tkinter.scrolledtext import ScrolledText
 WIDTH = 40
 SCRL_HEIGHT = 5
 SCRL_WIDTH = 20
+IMGS_HEIGHT = 5
+IMGS_WIDTH = 20
 
 class GUI:
     
@@ -15,7 +17,7 @@ class GUI:
         root.columnconfigure(0, weight=1)
         root.rowconfigure(0, weight=1)
 
-        mainframe = ttk.Frame(root, padding="3 3 12 12")
+        mainframe = ttk.Frame(root, padding="3 3 3 3")
         mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 
         mainframe.columnconfigure(1, weight=2)
@@ -54,17 +56,19 @@ class GUI:
         self.scrl_out_examples = ScrolledText   (mainframe, height=SCRL_HEIGHT, width=SCRL_WIDTH)
         self.scrl_out_related = ScrolledText    (mainframe, height=SCRL_HEIGHT, width=SCRL_WIDTH)
         self.scrl_out_synos = ScrolledText      (mainframe, height=SCRL_HEIGHT, width=SCRL_WIDTH)
+        self.frame_imgs = ttk.Frame(mainframe, height=IMGS_HEIGHT, width=IMGS_WIDTH)
         
         self.entry_query.grid         (column=1, row=0, sticky=(W, E))
-        self.entry_quick.grid         (column=1, row=8, sticky=(W, E))
         self.scrl_gen_defns_en.grid   (column=1, row=1, sticky=(W, E))
         self.scrl_gen_defns_ru.grid   (column=1, row=2, sticky=(W, E))
         self.scrl_gen_conjs_decls.grid(column=1, row=3, sticky=(W, E))
         self.scrl_gen_examples.grid   (column=1, row=4, sticky=(W, E))
         self.scrl_gen_related.grid    (column=1, row=5, sticky=(W, E))
         self.scrl_gen_synos.grid      (column=1, row=6, sticky=(W, E))
-        self.scrl_out_ru.grid         (column=5, row=1, sticky=(W, E))
-        self.scrl_out_en.grid         (column=5, row=2, sticky=(W, E))
+        self.frame_imgs.grid          (column=1, row=7, sticky=(W, E))
+        self.entry_quick.grid         (column=1, row=8, sticky=(W, E))
+        self.scrl_out_en.grid         (column=5, row=1, sticky=(W, E))
+        self.scrl_out_ru.grid         (column=5, row=2, sticky=(W, E))
         self.scrl_out_conjs_decls.grid(column=5, row=3, sticky=(W, E))
         self.scrl_out_examples.grid   (column=5, row=4, sticky=(W, E))
         self.scrl_out_related.grid    (column=5, row=5, sticky=(W, E))
@@ -79,26 +83,36 @@ class GUI:
         ttk.Label(mainframe, text="Generated Related:").grid(column=0, row=5, sticky=W)
         ttk.Label(mainframe, text="Generated Synonyms:").grid(column=0, row=6, sticky=W)
         ttk.Label(mainframe, text="Image Options:").grid(column=0, row=7, sticky=W)
-        ttk.Label(mainframe, text="Out Russian Field:").grid(column=4, row=1, sticky=W)
-        ttk.Label(mainframe, text="Out English Field:").grid(column=4, row=2, sticky=W)
+        ttk.Label(mainframe, text="Out English Field:").grid(column=4, row=1, sticky=W)
+        ttk.Label(mainframe, text="Out Russian Field:").grid(column=4, row=2, sticky=W)
         ttk.Label(mainframe, text="Out Conjugations/\nDeclensions Field:").grid(column=4, row=3, sticky=W)
         ttk.Label(mainframe, text="Out Examples Field:").grid(column=4, row=4, sticky=W)
         ttk.Label(mainframe, text="Out Related Field:").grid(column=4, row=5, sticky=W)
         ttk.Label(mainframe, text="Out Synonyms Field:").grid(column=4, row=6, sticky=W)
 
-        ttk.Button(mainframe, text="Generate", command=self.button_generate).grid(column=2, row=0)
-        ttk.Button(mainframe, text="Clear", command=self.button_clear).grid(column=3, row=0)
+        ttk.Button(mainframe, text="Generate", command=self.button_generate).grid(column=2, row=0, padx=10)
+        ttk.Button(mainframe, text="Clear", command=self.button_clear).grid(column=3, row=0, padx=10)
         ttk.Button(mainframe, text="Search", command=self.button_search).grid(column=2, row=8)
         ttk.Button(mainframe, text="Submit", command=self.button_submit).grid(column=3, row=9, pady=20)
-        #ttk.Button(mainframe, text="Update", command=self.test_cmd).grid(column=2, row=0)
-        #ttk.Entry(mainframe, width=WIDTH, textvariable=self.).grid(column=, row=, sticky=(W, E))
-
-        #entry_query = ttk.Entry(mainframe, width=7, textvariable=self.feet)
 
     def button_generate(self):
         print("generate")
 
     def button_clear(self):
+        self.stringvar_query.set("")
+        self.stringvar_quick.set("")
+        self.scrl_gen_defns_en.delete("1.0", END)
+        self.scrl_gen_defns_ru.delete("1.0", END)
+        self.scrl_gen_conjs_decls.delete("1.0", END)
+        self.scrl_gen_examples.delete("1.0", END)
+        self.scrl_gen_related.delete("1.0", END)
+        self.scrl_gen_synos.delete("1.0", END)
+        self.scrl_out_en.delete("1.0", END)
+        self.scrl_out_ru.delete("1.0", END)
+        self.scrl_out_conjs_decls.delete("1.0", END)
+        self.scrl_out_examples.delete("1.0", END)
+        self.scrl_out_related.delete("1.0", END)
+        self.scrl_out_synos.delete("1.0", END)
         print("clear")
 
     def button_search(self):
